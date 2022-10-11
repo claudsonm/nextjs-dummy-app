c5d5e7a568ee3f235f9c74dd4265b4c2d63783d1
Claudson Martins <claudson@outlook.com>
2022-10-11 08:57:31 -0300

    Update HeroSection.tsx

--- components/Sections/HeroSection.tsx
+++ components/Sections/HeroSection.tsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
             <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                 <div className="sm:text-center lg:text-left">
                     <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                        <span className="block xl:inline">Editado com sucesso</span>{' '}
+                        <span className="block xl:inline">Editado com sucesso agora no PR</span>{' '}
                         <span className="block text-indigo-600 xl:inline">online business</span>
                     </h1>
                     <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
